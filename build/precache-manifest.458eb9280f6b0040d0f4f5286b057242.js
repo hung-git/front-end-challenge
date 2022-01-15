04032167eadfa9aeb3d5c93507efa144db0a4d9d
Hung Nguyen <hdnguyen527@gmail.com>
2022-01-15 00:43:13 -0800

    pushing for deploy

---
+++ build/precache-manifest.458eb9280f6b0040d0f4f5286b057242.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6eac762484f0d7d41255c61684d85b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9497b5a79c7dfaf3198",
+    "url": "/static/css/main.266bed12.chunk.css"
+  },
+  {
+    "revision": "83cb06dd77a846369e27",
+    "url": "/static/js/2.87792127.chunk.js"
+  },
+  {
+    "revision": "284f6bda63542c74b9afe90b1ae532c6",
+    "url": "/static/js/2.87792127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9497b5a79c7dfaf3198",
+    "url": "/static/js/main.b8226b7e.chunk.js"
+  },
+  {
+    "revision": "4dfabf60636c93a1e00a",
+    "url": "/static/js/runtime-main.e4585da2.js"
+  },
+  {
+    "revision": "c627b7408ba5f3413ba896d3d7c34e7a",
+    "url": "/static/media/HUNG_NGUYEN.c627b740.jpg"
+  }
+]);
